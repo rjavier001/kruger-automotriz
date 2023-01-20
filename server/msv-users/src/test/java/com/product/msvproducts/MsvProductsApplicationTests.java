82927c7f2bad5cd47d2477894f87662a82b135f8
Javier Vallejo <rjavier001@gmail.com>
2023-01-20 15:22:10 -0500

    msv-users project created

---
+++ server/msv-users/src/test/java/com/product/msvproducts/MsvProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.msvproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
