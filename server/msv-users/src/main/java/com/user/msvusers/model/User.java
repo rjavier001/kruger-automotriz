b1feb0f26008b9bdd4c0e55bcc75689a85b59c21
Javier Vallejo <rjavier001@gmail.com>
2023-01-20 17:32:02 -0500

    msv-users complete

--- server/msv-users/src/main/java/com/user/msvusers/model/User.java
+++ server/msv-users/src/main/java/com/user/msvusers/model/User.java
@@ -1,11 +1,10 @@
 package com.user.msvusers.model;
 
-
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -15,17 +14,17 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-//  @NotEmpty(message = "Name should not be empty")
-//  @NotNull(message = "Name should not be null")
+  @NotEmpty(message = "Name should not be empty")
+  @NotNull(message = "Name should not be null")
   private String name;
 
-//  @NotEmpty(message = "Email should not be empty")
-//  @NotNull(message = "Email should not be null")
+  @NotEmpty(message = "Email should not be empty")
+  @NotNull(message = "Email should not be null")
   @Column(unique = true)
   private String email;
 
-//  @NotEmpty(message = "Password should not be empty")
-//  @NotNull(message = "Password should not be null")
+  @NotEmpty(message = "Password should not be empty")
+  @NotNull(message = "Password should not be null")
   private String password;
 
 }
