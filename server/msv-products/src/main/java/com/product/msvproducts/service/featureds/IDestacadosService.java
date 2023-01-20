3bb93cfeae7a322abdc9c9ad909124ff5cc8be61
KevinVeliz <mario-veliz258934@hotmail.com>
2023-01-19 23:55:39 -0500

    Field validation

---
+++ server/msv-products/src/main/java/com/product/msvproducts/service/featureds/IDestacadosService.java
@@ -0,0 +1,15 @@
+package com.product.msvproducts.service.featureds;
+
+
+import com.product.msvproducts.entity.FeaturedEntity;
+
+import java.util.List;
+
+public interface IDestacadosService {
+    public List<FeaturedEntity> listAllDestacados();
+    public FeaturedEntity getDestacados(Long id);
+
+    public FeaturedEntity createDestacados(FeaturedEntity destacados);
+    public FeaturedEntity updateDestacados(FeaturedEntity destacados);
+    public void deleteDestacados(Long id);
+}
