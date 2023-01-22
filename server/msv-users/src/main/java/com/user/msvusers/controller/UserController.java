8d4dadaf22d835144baee3d077692c87d2b20406
eddyjav <eddy.j.av@gmail.com>
2023-01-22 17:40:30 -0500

    :car: FeignClient User_Order - AssignOrder To User & CreateNewOrder to LogedUser

--- server/msv-users/src/main/java/com/user/msvusers/controller/UserController.java
+++ server/msv-users/src/main/java/com/user/msvusers/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.user.msvusers.controller;
 
+import com.user.msvusers.model.Order;
 import com.user.msvusers.model.entity.User;
 import com.user.msvusers.service.IUserService;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -59,4 +62,50 @@ public class UserController {
     }
     return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
   }
+
+  @PutMapping("/assign-order/{userId}")
+  public ResponseEntity<?> assignOrder(@RequestBody Order order, @PathVariable Long userId){
+    Optional<Order> o;
+    try{
+      o = service.assignOrder(order, userId);
+    } catch (FeignException e){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("mensaje","No existe el usuario por el ID o error en la comunicacion"+ e.getMessage()));
+    }
+    if(o.isPresent()){
+      return ResponseEntity.status(HttpStatus.CREATED).body(o.get());
+    }
+    return  ResponseEntity.notFound().build();
+  }
+
+  @PostMapping("/create-order/{userId}")
+  public ResponseEntity<?> createOrder(@RequestBody Order order, @PathVariable Long userId){
+    Optional<Order> o;
+    try{
+      o = service.createOrder(order, userId);
+    } catch (FeignException e){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+              .body(Collections.singletonMap("mensaje","No se pudo crear la orden o error en la comunicacion"+ e.getMessage()));
+    }
+    if(o.isPresent()){
+      return ResponseEntity.status(HttpStatus.CREATED).body(o.get());
+    }
+    return  ResponseEntity.notFound().build();
+  }
+
+  @DeleteMapping("/delete-order/{userId}")
+  public ResponseEntity<?> deleteOrder(@RequestBody Order order, @PathVariable Long userId){
+    Optional<Order> o;
+    try{
+      o = service.deleteOrder(order, userId);
+    } catch (FeignException e){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("mensaje","No existe el usuario por el ID o error en la comunicacion"+ e.getMessage()));
+    }
+    if(o.isPresent()){
+      return ResponseEntity.status(HttpStatus.OK).body(o.get());
+    }
+    return  ResponseEntity.notFound().build();
+  }
+
+
+
 }
