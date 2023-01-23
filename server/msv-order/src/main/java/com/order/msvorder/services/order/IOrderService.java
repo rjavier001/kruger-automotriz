42191aaf30672d27dd3ddfee85df822c09d405b3
eddyjav <eddy.j.av@gmail.com>
2023-01-23 16:41:30 -0500

    :car: InterfaceOrder/+Methds-create-delete-Products

--- server/msv-order/src/main/java/com/order/msvorder/services/order/IOrderService.java
+++ server/msv-order/src/main/java/com/order/msvorder/services/order/IOrderService.java
@@ -1,8 +1,11 @@
 package com.order.msvorder.services.order;
 
 import com.order.msvorder.entity.Order;
+import com.order.msvorder.model.Product;
+
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrderService {
 
@@ -17,4 +20,13 @@ public interface IOrderService {
     public Order getOrder(Long id);
 
     List<Order> listByIds(Iterable<Long> ids);
+
+    //Metodos remotos
+    //Recibe el objeto dle producto y el id de la orden
+    Optional<Product> assignProduct(Product product, Long orderId);
+    //Este metodo no se lo voy  a implementar , crea un producto nuevo
+    //Optional<Product> createProduct(Product product, Long orderId);
+    Optional<Product> deleteProduct(Product product, Long orderId);
+
+
 }
