403709005014f3edb4c2bd2fc2a40982e31f3cc3
Javier Vallejo <rjavier001@gmail.com>
2023-01-23 22:23:35 -0500

    Company interface added

--- server/msv-users/src/main/java/com/user/msvusers/service/ICompanyService.java
+++ server/msv-users/src/main/java/com/user/msvusers/service/ICompanyService.java
@@ -1,16 +1,16 @@
-//package com.user.msvusers.service;
-//
-//import com.user.msvusers.model.entity.Company;
-//import com.user.msvusers.model.entity.User;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//
-//public interface ICompanyService {
-//  List<Company> findAll();
-//  Optional<Company> findById(Long id);
-//  Company save(Company company);
-//  void delete(Long id);
-//
-//}
+package com.user.msvusers.service;
+
+import com.user.msvusers.model.entity.Company;
+import com.user.msvusers.model.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface ICompanyService {
+  List<Company> findAll();
+  Optional<Company> findById(Long id);
+  Company save(Company company);
+  void delete(Long id);
+
+}
