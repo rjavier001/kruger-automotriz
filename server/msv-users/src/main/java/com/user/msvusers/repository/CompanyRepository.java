e9d4fd4ada5ef2f9613968968cba352db9545ce2
Javier Vallejo <rjavier001@gmail.com>
2023-01-23 22:22:30 -0500

    Company repository added

--- server/msv-users/src/main/java/com/user/msvusers/repository/CompanyRepository.java
+++ server/msv-users/src/main/java/com/user/msvusers/repository/CompanyRepository.java
@@ -1,8 +1,8 @@
-//package com.user.msvusers.repository;
-//
-//import com.user.msvusers.model.entity.Company;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface CompanyRepository extends CrudRepository<Company,Long> {
-//
-//}
+package com.user.msvusers.repository;
+
+import com.user.msvusers.model.entity.Company;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company,Long> {
+
+}
