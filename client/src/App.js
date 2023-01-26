07c8233c83a394ce57bd0ab65b6fd488e4044b9f
David Lozada <davsh47@hotmail.com>
2023-01-26 11:18:30 +0300

    checkoutPage
    frontend checkout page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Screens/Home";
 import Shop from "./Screens/Shop";
+import CheckOutPage from "./Screens/CheckOutPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/checkout" element={<CheckOutPage />} />
     </Routes>
   );
 }
