a23a1a56d4080b7c55206160a394e22d789109e8
KevinVeliz <mario-veliz258934@hotmail.com>
2023-01-25 21:08:30 -0500

    Fix

--- client/automotriz-front/src/Components/CardComp.jsx
+++ client/automotriz-front/src/Components/CardComp.jsx
@@ -4,29 +4,29 @@ import { Avatar, IconButton,CardMedia } from "@mui/material";
 import { Container } from '@mui/system';
 
 const CardComp = (props) => {
-    const { avatarUrl, title, subtitle, description, imageUrl } = props;
+  
+  const { avatarUrl, title, subtitle, description, imageUrl } = props;
   return (
     <Container>
-    <Card sx={{ maxWidth: 345 }} >
-      <CardMedia
-        sx={{ height: 140 }}
-        image={imageUrl}
-        title="green iguana"
-        />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {subtitle}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-        </Container>
+      <Card sx={{ maxWidth: 345 }} >
+        <CardMedia
+          sx={{ height: 140 }}
+          image={imageUrl}
+          title="product image"
+          />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {subtitle}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="medium" variant="contained">View More</Button>
+        </CardActions>
+      </Card>
+    </Container>
   )
 }
 
