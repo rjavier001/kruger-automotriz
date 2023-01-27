0f4c016aa75548ef5af247ec16d1a5842e93dde3
Javier Yanez <eddy.j.av@gmail.com>
2023-01-27 15:03:37 -0500

    :car: Fix Home/Shop & +Img

--- client/src/Screens/Shop.jsx
+++ client/src/Screens/Shop.jsx
@@ -1,60 +1,71 @@
-import { Box, Grid } from '@mui/material'
-import { Container } from '@mui/system'
-import React from 'react'
-import CardComp from '../Components/CardComp'
-import CarouselComp from '../Components/CarouselComp'
+import { Box, Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import CardComp from "../Components/CardComp";
+import CarouselComp from "../Components/CarouselComp";
 
+const list = [
+  {
+    id: 1,
+    name: "test",
+    subtitulo: "esto es un subtitulo",
+    image:
+      "https://res.cloudinary.com/dhaklfydk/image/upload/v1674796029/kruger-products/correa3_arlmh5.png",
+  },
+  {
+    id: 2,
+    name: "test1",
+    subtitulo: "esto es un subtitulo2",
+    image:
+      "https://res.cloudinary.com/dhaklfydk/image/upload/v1674796027/kruger-products/bujia3_p3bn0e.png",
+  },
+  {
+    id: 3,
+    name: "test2",
+    subtitulo: "esto es un subtitulo2",
+    image:
+      "https://res.cloudinary.com/dhaklfydk/image/upload/v1674796022/kruger-products/alevas1_aelyxq.png",
+  },
+  {
+    id: 4,
+    name: "test3",
+    subtitulo: "esto es un subtitulo2",
+    image:
+      "https://res.cloudinary.com/dhaklfydk/image/upload/v1674796017/kruger-products/faro12_ucdlcv.png",
+  },
+];
 
-
-const list=[
-    {
-        "id":1,
-        "name":"test",
-        "subtitulo":"esto es un subtitulo",
-        "image": "https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-        "id":2,
-        "name":"test1",
-        "subtitulo":"esto es un subtitulo2",
-        "image": "https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-        "id":3,
-        "name":"test2",
-        "subtitulo":"esto es un subtitulo2",
-        "image": "https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    },
-    {
-        "id":4,
-        "name":"test3",
-        "subtitulo":"esto es un subtitulo2",
-        "image": "https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-    }
-]
-
-let ima = list.map((item)=>(item.image))
-console.log(ima)
-
+let ima = list.map((item) => item.image);
+console.log(ima);
 
 const Shop = () => {
   return (
     <>
-    
-    <CarouselComp items={ima}/>
+      <CarouselComp items={ima} />
 
-
-    <Container>
-    <Grid item container spacing={2} marginTop="2px"  >
-        {list.map((item,i)=>(
-            <Grid key={i} item xs={12} md={4} sm={6} spacing={2} justify="center" >
-                <CardComp title={item.name} subtitle={item.subtitulo} imageUrl={"https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}/>
+      <Container>
+        <Grid item container spacing={2} marginTop="2px">
+          {list.map((item, i) => (
+            <Grid
+              key={i}
+              item
+              xs={12}
+              md={4}
+              sm={6}
+              spacing={2}
+              justify="center"
+            >
+              <CardComp
+                title={item.name}
+                subtitle={item.subtitulo}
+                imageUrl={item.image}
+              />
             </Grid>
-        ))}
-    </Grid>
-    </Container>
+          ))}
+        </Grid>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
