9711e96f8c53e81116753d6833c46200764f988a
Javier Vallejo <rjavier001@gmail.com>
2023-01-27 15:28:56 -0500

    themeMode provider added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
+import { ThemeProvider } from "@mui/material/styles";
 import "./App.css";
+import { useSelector } from "react-redux";
+import themeConfigs from "./configs/theme.configs";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Screens/Home";
 import Shop from "./Screens/Shop";
 import CheckOutPage from "./Screens/CheckOutPage";
 
 function App() {
+  const { themeMode } = useSelector((state) => state.themeMode);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/checkout" element={<CheckOutPage />} />
-    </Routes>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
