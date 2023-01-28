73c9bbe9777b757febec9b361dcca37b6efb6653
Javier Vallejo <rjavier001@gmail.com>
2023-01-28 00:49:56 -0500

    store updated

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import reduxThunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+import themeModeSlice from "./features/themeModeSlice";
 
-import rootReducer from "./root-reducer";
-
-const middleware = [reduxThunk];
-
-if (process.env.NODE_ENV === "development") {
-  middleware.push(logger);
-}
-
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = configureStore({
+  reducer: {
+    themeMode: themeModeSlice,
+  },
+});
 
 export default store;
