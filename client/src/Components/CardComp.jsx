c6ef6f7131d43d092bf4c2e8f9a3ca2208fe7360
David Lozada <davsh47@hotmail.com>
2023-01-30 18:56:50 +0300

    Product information

--- client/src/Components/CardComp.jsx
+++ client/src/Components/CardComp.jsx
@@ -10,15 +10,13 @@ import React from "react";
 import { CardMedia } from "@mui/material";
 import { Container } from "@mui/system";
 import uiConfigs from "../configs/ui.configs";
+import { NavLink } from "react-router-dom";
 
 const CardComp = ({ props }) => {
-  const { name, price, photoUrl,stock,description } = props;
+  const { name, price, photoUrl, stock, description } = props;
   return (
     <Container>
-      <Card
-        raised
-        sx={uiConfigs.box}
-      >
+      <Card raised sx={uiConfigs.box}>
         <CardMedia
           component="img"
           height="250"
@@ -27,34 +25,49 @@ const CardComp = ({ props }) => {
           title="product image"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div" sx={uiConfigs.item}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={uiConfigs.item}
+          >
             {name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             {description}
           </Typography>
-          <Stack direction="row" spacing={15} alignItems="center" sx={uiConfigs.card}>
-              <Typography variant="body2" color="text.secondary" sx={uiConfigs.text}>
-                Price: ${price}
-              </Typography>
-              <Typography variant="body2" color="text.secondary" sx={uiConfigs.text}>
-                Stock: {stock}
-              </Typography>
+          <Stack
+            direction="row"
+            spacing={15}
+            alignItems="center"
+            sx={uiConfigs.card}
+          >
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={uiConfigs.text}
+            >
+              Price: ${price}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={uiConfigs.text}
+            >
+              Stock: {stock}
+            </Typography>
           </Stack>
         </CardContent>
         <CardActions sx={uiConfigs.button}>
-          <Button
-            color="secondary"
-            size="medium"
-            variant="contained"
-          >
-            View More
-          </Button>
+          <NavLink to={"/details"} style={{ textDecoration: "none" }}>
+            <Button color="secondary" size="medium" variant="contained">
+              View More
+            </Button>
+          </NavLink>
         </CardActions>
       </Card>
     </Container>
   );
 };
-
 
 export default CardComp;
---
+++ client/src/screens/ProductDetailPage.jsx
@@ -0,0 +1,99 @@
+import {
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
+import React, { useState } from "react";
+import Image from "mui-image";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import CompanyBenefitsComp from "../Components/CompanyBenefitsComp";
+
+const ProductDetailPage = () => {
+  const [counter, setCounter] = useState(1);
+  /* Control de counter no sea menor a 0 */
+  const addCouter = () => {
+    if (counter >= 0) {
+      setCounter(counter + 1);
+    }
+  };
+
+  const resCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <Container>
+      <Grid
+        container
+        spacing={2}
+        marginTop="2px"
+        sx={{ minHeight: 450, paddingTop: 5 }}
+      >
+        <Grid item xs={12} md={8} sm={6}>
+          <Image
+            alt="product"
+            showLoading={false}
+            errorIcon={true}
+            fit={"cover"}
+            height={450}
+            width={"100%"}
+            src="https://picsum.photos/id/674/2000"
+          />
+          {/* RIGHT PANEL */}
+        </Grid>
+        <Grid item xs={12} md={4}>
+          Kruger Motors
+          <Stack
+            direction={"column"}
+            sx={{
+              flex: 1,
+              height: 450,
+              alignItems: "space-around",
+              justifyContent: "space-around",
+            }}
+          >
+            <Typography variant="h6">Nombre de Parte</Typography>
+            <Typography variant="h7">255.30$</Typography>
+            <Typography variant="h7">Nro de Parte: 1745646</Typography>
+            <Typography variant="h7">Stock: 25</Typography>
+
+            <Stack direction={"row"} sx={styles.add}>
+              <IconButton size="small" onClick={() => resCounter()}>
+                <RemoveIcon />
+              </IconButton>
+              <Typography>{counter}</Typography>
+              <IconButton size="small" onClick={() => addCouter()}>
+                <AddIcon />
+              </IconButton>
+            </Stack>
+            <Stack
+              direction={{ md: "row", sx: "column" }}
+              spacing={2}
+              sx={{ justifyContent: "space-around" }}
+            >
+              <Button variant="contained">Agregar al carrito</Button>
+              <Button variant="contained" color="secondary">
+                Comprar
+              </Button>
+            </Stack>
+          </Stack>
+        </Grid>
+        CompanyBenefitsComp
+      </Grid>
+    </Container>
+  );
+};
+
+export default ProductDetailPage;
+
+export const styles = {
+  add: {
+    alignItems: "center",
+  },
+  rightPanel: {},
+};
