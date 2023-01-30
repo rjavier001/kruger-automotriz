d6a6b086a2ce0e0f5d846e388bf3cb5ad7e9f9ee
Javier Yanez <eddy.j.av@gmail.com>
2023-01-29 19:29:44 -0500

    :car: DividerBrand add brans logos

--- client/src/Components/common/DividerBrand.jsx
+++ client/src/Components/common/DividerBrand.jsx
@@ -7,6 +7,11 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
 import Logo from "../../Assets/LOGO_KRUGERMOTORS.png";
+import chevrolet_logo from "../../Assets/Logos/chevrolet_logo.png";
+import fiat_logo from "../../Assets/Logos/fiat_logo.png";
+import ford_logo from "../../Assets/Logos/ford_logo.png";
+import kia_logo from "../../Assets/Logos/kia_logo.png";
+import volkswagen_logo from "../../Assets/Logos/volkswagen_logo.png";
 
 import { Box } from "@mui/system";
 
@@ -19,7 +24,13 @@ const DividerBrand = () => {
       spacing={10}
       alignItems="center"
       justifyContent="space-evenly"
-    ></Stack>
+    >
+      <Image width="auto" height={80} src={chevrolet_logo} />
+      <Image width="auto" src={fiat_logo} />
+      <Image width="auto" src={kia_logo} />
+      <Image width="auto" src={ford_logo} />
+      <Image width="auto" src={volkswagen_logo} />
+    </Stack>
   );
 };
 
