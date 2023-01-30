631e4193328e2be6635c64cf9630a4eb210d4162
David Lozada <davsh47@hotmail.com>
2023-01-30 20:10:25 +0300

    fixed capitals

--- client/src/Components/common/DividerBrand.jsx
+++ client/src/Components/common/DividerBrand.jsx
@@ -6,12 +6,12 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
-import Logo from "../../Assets/LOGO_KRUGERMOTORS.png";
-import chevrolet_logo from "../../Assets/Logos/chevrolet_logo.png";
-import fiat_logo from "../../Assets/Logos/fiat_logo.png";
-import ford_logo from "../../Assets/Logos/ford_logo.png";
-import kia_logo from "../../Assets/Logos/kia_logo.png";
-import volkswagen_logo from "../../Assets/Logos/volkswagen_logo.png";
+import Logo from "../../assets/LOGO_KRUGERMOTORS.png";
+import chevrolet_logo from "../../assets/Logos/chevrolet_logo.png";
+import fiat_logo from "../../assets/Logos/fiat_logo.png";
+import ford_logo from "../../assets/Logos/ford_logo.png";
+import kia_logo from "../../assets/Logos/kia_logo.png";
+import volkswagen_logo from "../../assets/Logos/volkswagen_logo.png";
 
 import { Box } from "@mui/system";
 
--- client/src/Components/common/Sidebar.jsx
+++ client/src/Components/common/Sidebar.jsx
@@ -10,13 +10,13 @@ import {
 } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import menuConfigs from "../../Configs/menu.configs";
+import menuConfigs from "../../configs/menu.configs";
 // import Logo from "./Logo";
-import uiConfigs from "../../Configs/ui.configs";
+import uiConfigs from "../../configs/ui.configs";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
 
-import { themeModes } from "../../Configs/theme.configs";
+import { themeModes } from "../../configs/theme.configs";
 import { setThemeMode } from "../../redux/features/themeModeSlice";
 
 const Sidebar = ({ open, toggleSidebar }) => {
