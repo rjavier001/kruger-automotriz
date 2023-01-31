92c7a64c28587a514135446cc0e745178a01af64
Javier Yanez <eddy.j.av@gmail.com>
2023-01-31 10:59:52 -0500

    :car: Modf Footer/Img&Spacing

--- client/src/Components/common/Footer.jsx
+++ client/src/Components/common/Footer.jsx
@@ -1,4 +1,4 @@
-import { IconButton, Stack, Typography } from "@mui/material";
+import { Divider, IconButton, Stack, styled, Typography } from "@mui/material";
 
 import Image from "mui-image";
 
@@ -10,36 +10,102 @@ import Logo from "../../Assets/LOGO_KRUGERMOTORS.png";
 import { Box } from "@mui/system";
 
 const Footer = () => {
+  const Div = styled("div")(({ theme }) => ({
+    ...theme.typography.button,
+    textAlign: "end",
+  }));
+
+  const Img = styled("img")(({ theme }) => ({
+    width: "60%",
+    position: "relative",
+    objectFit: "contain",
+    top: "-100%",
+    left: "22%",
+  }));
+
   return (
-    <Stack
-      sx={{ marginTop: "40px", background: "#ff910d" }}
-      height={200}
-      direction="row"
-      spacing={12}
-      justifyContent="space-evenly"
-    >
-      <Stack direction="row" spacing={2}>
-        <Typography sx={{ paddingTop: "30px" }} variant="h3">
-          LLAMANOS <br /> 123 456 789
-        </Typography>
-      </Stack>
-      <Stack height={180} alignItems={"flex-start"}>
-        <Typography sx={{ paddingLeft: "10px" }} variant="h4">
-          Redes
-        </Typography>
+    <Stack>
+      <Stack
+        sx={{ marginTop: "40px", background: "#ff910d" }}
+        height={200}
+        direction="row"
+        spacing={12}
+        justifyContent="space-evenly"
+      >
+        <Stack direction="column" spacing={0}>
+          <Stack direction="row" spacing={0}>
+            <Stack
+              sx={{
+                width: "10px",
+                marginTop: "10px",
+                background: "#00b040",
+              }}
+            >
+              &nbsp;
+            </Stack>
+            <Typography
+              variant="button"
+              sx={{ paddingTop: "10px", color: "#ffffff" }}
+            >
+              &nbsp; Ventas y Cotizaciones
+            </Typography>
+          </Stack>
 
-        <IconButton>
-          <FacebookIcon /> Facebook
-        </IconButton>
-        <IconButton>
-          <InstagramIcon /> Instagram
-        </IconButton>
-        <IconButton>
-          <TwitterIcon /> Twitter
-        </IconButton>
-      </Stack>
+          <Typography variant="h4">
+            LLAMANOS
+            <br />
+            123 456 789
+          </Typography>
+          <Div>
+            Lun - Vie: 08:00 AM - 18:00 PM
+            <br />
+            Sab: 08:00 AM - 12:00 PM
+          </Div>
+        </Stack>
+        <Stack height={180} alignItems={"flex-start"}>
+          <Typography
+            sx={{ paddingLeft: "10px", paddingTop: "10px" }}
+            variant="h4"
+          >
+            Redes
+          </Typography>
+          <IconButton>
+            <FacebookIcon /> Facebook
+          </IconButton>
+          <IconButton>
+            <InstagramIcon /> Instagram
+          </IconButton>
+          <IconButton>
+            <TwitterIcon /> Twitter
+          </IconButton>
+        </Stack>
 
-      <Image width="auto" src={Logo} />
+        <Stack width="auto" direction="column" spacing={0}>
+          <Img src={Logo} alt="krugermotors" />
+          {/* <Image
+            src={Logo}
+            sx={{
+              width: "20px",
+              position: "relative",
+              objectFit: "contain",
+              top: "-70%",
+            }}
+          /> */}
+          <Typography
+            variant="button"
+            sx={{
+              textAlign: "center",
+              width: "60%",
+              position: "relative",
+              objectFit: "contain",
+              top: "-100%",
+              left: "22%",
+            }}
+          >
+            Repuestos 100% confiables
+          </Typography>
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
