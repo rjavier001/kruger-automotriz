b6369dd6129c65e02411dc74b3b1edbeeedeb58f
Javier Yanez <eddy.j.av@gmail.com>
2023-01-31 17:52:50 -0500

    :car: Add DeliveryComp & Mod./Fix Footr

--- client/src/components/common/DeliveryComp.jsx
+++ client/src/components/common/DeliveryComp.jsx
@@ -1,13 +1,62 @@
-import { Stack } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Image from "mui-image";
 
-import Delivery from "../../Assets/KrugerEntregas.png";
+import Delivery from "../../assets/KrugerEntregas.png";
 
 const DeliveryComp = () => {
   return (
     <Stack>
-      <Stack direction="column" spacing={0}>
-        <Image src={Delivery} height="50%" width="50%" />
+      <Typography
+        textAlign="center"
+        variant="h3"
+        component="div"
+        sx={{
+          paddingTop: "40px",
+          paddingBottom: "20px",
+          fontFamily: "Staatliches",
+        }}
+      >
+        Entregas a Domicilio
+      </Typography>
+      <Stack justifyContent={"center"} alignItems={"center"}>
+        <Card
+          sx={{
+            display: "flex",
+            width: "80%",
+          }}
+        >
+          <CardMedia
+            component="img"
+            // height="150"
+            sx={{
+              width: 230,
+            }}
+            image={Delivery}
+            alt="Delivery"
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              paddingRight={42}
+              textAlign="justify"
+            >
+              Realizamos envíos a nivel nacional por medio de la empresa Servi
+              Entrega o Larrcourier, si te encuentras en la ciudad de Quito
+              puedes retirar tu compra en nuestra empresa. <br /> Si tu compra
+              llega a más de 300 $ el envío es gratis!!
+            </Typography>
+          </CardContent>
+        </Card>
       </Stack>
     </Stack>
   );
