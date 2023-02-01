e78b12b50a55d192e53d470f53bc3082498da4b9
David Lozada <davsh47@hotmail.com>
2023-02-01 15:08:16 +0300

    PaymentPage

---
+++ client/src/screens/PaymentPage.jsx
@@ -0,0 +1,192 @@
+import {
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  TextField,
+  Button,
+  MenuItem,
+  Box,
+  Divider,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { useSelector, useDispatch } from "react-redux";
+import Image from "mui-image";
+const PaymentPage = () => {
+  const cart = useSelector((state) => state.cart);
+  const payments = [
+    {
+      value: "Paypall",
+      label: "Paypall",
+    },
+    {
+      value: "Tarjeta",
+      label: "Tarjeta",
+    },
+  ];
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      apellido: "",
+      email: "",
+      address: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <Container sx={styles.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h6">1.-Confirma tu orden:</Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Typography sx={styles.headers}>Repuesto:</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography sx={styles.headers}>Subtotal:</Typography>
+            </Grid>
+          </Grid>
+
+          {cart.cartItems?.map((cartItem) => (
+            <>
+              <Grid
+                container
+                spacing={2}
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Grid item xs={12} md={6}>
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Image
+                      alt="product"
+                      showLoading={false}
+                      errorIcon={true}
+                      fit={"contain"}
+                      src={cartItem.photoUrl}
+                      height={80}
+                      width={80}
+                      duration={1000}
+                    />
+                    <Typography>{cartItem.cartQuantity}x</Typography>
+                    {cartItem.name}
+                  </Box>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <Typography sx={styles.price}>
+                    ${cartItem.price * cartItem.cartQuantity}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Divider sx={{ marginY: 2 }} />
+            </>
+          ))}
+          <Stack sx={styles.stackRoot}>
+            <Typography sx={styles.headers}>Total:</Typography>
+            <Typography sx={styles.headers}>{cart.cartTotalAmount}$</Typography>
+          </Stack>
+        </Grid>
+
+        <Grid container item xs={12} md={6}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6">2.-Escoge un metodo de pago:</Typography>
+
+            <TextField
+              id="outlined-select-currency"
+              select
+              label="Select"
+              defaultValue="EUR"
+              helperText="Please select your currency"
+            >
+              {payments.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Typography variant="h6">
+            3.-Ingresa tu informacion de pago:
+          </Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container spacing={2} mb={2}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  id="name"
+                  label="Nombre"
+                  name="name"
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  id="apellido"
+                  label="Apellido"
+                  name="apellido"
+                  onChange={formik.handleChange}
+                  value={formik.values.apellido}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} md={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  type={"email"}
+                  name="email"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                />
+              </Grid>
+              <Grid item xs={12} md={12}>
+                <TextField
+                  required
+                  id="address"
+                  label="Direccion"
+                  name="address"
+                  onChange={formik.handleChange}
+                  value={formik.values.address}
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default PaymentPage;
+
+export const styles = {
+  container: {
+    marginTop: 2,
+  },
+  headers: {
+    fontWeight: "bold",
+    fontSize: "15px",
+    textAlign: "center",
+  },
+  price: {
+    textAlign: "center",
+  },
+};
