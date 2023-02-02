e0c98cb41c60ec13b5e137acf49318ea5e8f6a1d
Javier Vallejo <rjavier001@gmail.com>
2023-02-02 15:03:36 -0500

    auth-server complete

--- server/auth-server/src/main/java/com/kruger/authserver/AuthServerApplication.java
+++ server/auth-server/src/main/java/com/kruger/authserver/AuthServerApplication.java
@@ -2,8 +2,10 @@ package com.kruger.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
