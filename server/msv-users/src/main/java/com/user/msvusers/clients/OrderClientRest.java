a7a1518d85c3b780ad07e849a682e2d2d2bbc828
Javier Yanez <eddy.j.av@gmail.com>
2023-02-02 17:58:50 -0500

    :car: Add DockerFile - msv-order && dockerized

--- server/msv-users/src/main/java/com/user/msvusers/clients/OrderClientRest.java
+++ server/msv-users/src/main/java/com/user/msvusers/clients/OrderClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "msv-order", url = "localhost:8003/api/orders")
+@FeignClient(name = "msv-order", url = "host.docker.internal:8003/api/orders")
 public interface OrderClientRest {
 
     @GetMapping(value = "/{id}")
