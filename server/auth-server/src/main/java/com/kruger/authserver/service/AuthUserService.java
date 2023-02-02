461bc07f0b3490c8118fbcae6aebf5ad4238ea28
Javier Vallejo <rjavier001@gmail.com>
2023-02-02 15:03:36 -0500

    auth-server complete

---
+++ server/auth-server/src/main/java/com/kruger/authserver/service/AuthUserService.java
@@ -0,0 +1,56 @@
+package com.kruger.authserver.service;
+
+
+import com.kruger.authserver.dto.AuthUserDto;
+import com.kruger.authserver.dto.TokenDto;
+import com.kruger.authserver.entity.AuthUser;
+import com.kruger.authserver.repository.AuthUserRepository;
+import com.kruger.authserver.security.JwtProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthUserService {
+
+  @Autowired
+  AuthUserRepository authUserRepository;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Autowired
+  JwtProvider jwtProvider;
+
+  public AuthUser save(AuthUserDto dto) {
+    Optional<AuthUser> user = authUserRepository.findByUserName(dto.getUserName());
+    if(user.isPresent())
+      return null;
+    String password = passwordEncoder.encode(dto.getPassword());
+    AuthUser authUser = AuthUser.builder()
+        .userName(dto.getUserName())
+        .password(password)
+        .build();
+    return authUserRepository.save(authUser);
+  }
+
+  public TokenDto login(AuthUserDto dto) {
+    Optional<AuthUser> user = authUserRepository.findByUserName(dto.getUserName());
+    if(!user.isPresent())
+      return null;
+    if(passwordEncoder.matches(dto.getPassword(), user.get().getPassword()))
+      return new TokenDto(jwtProvider.createToken(user.get()));
+    return null;
+  }
+
+  public TokenDto validate(String token) {
+    if(!jwtProvider.validate(token))
+      return null;
+    String username = jwtProvider.getUserNameFromToken(token);
+    if(!authUserRepository.findByUserName(username).isPresent())
+      return null;
+    return new TokenDto(token);
+  }
+}
