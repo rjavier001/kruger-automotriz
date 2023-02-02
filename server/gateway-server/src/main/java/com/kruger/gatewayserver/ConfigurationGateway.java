7f00d9017fe5fb18a2c97d887bd32405ba6f7f29
Javier Vallejo <rjavier001@gmail.com>
2023-02-02 13:57:07 -0500

    gateway-server updated

--- server/gateway-server/src/main/java/com/kruger/gatewayserver/ConfigurationGateway.java
+++ server/gateway-server/src/main/java/com/kruger/gatewayserver/ConfigurationGateway.java
@@ -1,17 +1,17 @@
-package com.kruger.gatewayserver;
-
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ConfigurationGateway {
- @Bean
- public RouteLocator configureRoute(RouteLocatorBuilder builder) {
-   return builder.routes()       
-       .route("users", r->r.path("/api/users/**").uri("lb://msv-users"))
-       .build();
- }
-}
+//package com.kruger.gatewayserver;
+//
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ConfigurationGateway {
+// @Bean
+// public RouteLocator configureRoute(RouteLocatorBuilder builder) {
+//   return builder.routes()
+//       .route("users", r->r.path("/api/users/**").uri("lb://msv-users"))
+//       .build();
+// }
+//}
