0c6b19696b32f96156f210b4ecf9c98837eaa352
David Lozada <davsh47@hotmail.com>
2023-02-04 14:37:55 +0300

    Navbar Style

--- client/src/components/common/ReviewComp.jsx
+++ client/src/components/common/ReviewComp.jsx
@@ -3,12 +3,26 @@ import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import Divider from "@mui/material/Divider";
 import Button from "@mui/material/Button";
 import { Container, Stack } from "@mui/system";
 import ModalPaymentsComp from "./ModalPaymentsComp";
+import axios from "axios";
 
-const ReviewComp = () => {
+const ReviewComp = ({ props }) => {
   const [value, setValue] = useState();
+  const [rating, setRating] = useState();
+
+  const onImputChange = (e) => {
+    setRating({ ...rating, [e.target.name]: e.target.value });
+  };
+
+  const handleReview = async (e) => {
+    e.preventDefault();
+    /* await axios.post("http://localhost:8080/province", provinces); */
+    /*   navigate("/"); */
+  };
+
   return (
     <Container>
       <Stack
@@ -21,24 +35,40 @@ const ReviewComp = () => {
         <Typography component="legend" variant="h6">
           Calificaciones:
         </Typography>
-        <Typography component="legend">Aun no hay Calificaciones </Typography>
+        {props.review.length === 0 && (
+          <Typography component="legend">Aun no hay Calificaciones </Typography>
+        )}
+        {props.review.map((reviews) => (
+          <>
+            <Rating name="read-only" value={reviews.stars} readOnly />
+            <Typography>{reviews.description}</Typography>
+            <Divider sx={{ marginY: 2 }} />
+          </>
+        ))}
+        <Typography variant="h6">Dejanos tu opinion: </Typography>
         <Rating
-          name="simple-controlled"
           value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
+          name={"stars"}
+          onChange={(e) => {
+            onImputChange(e);
           }}
         />
         <TextField
           id="outlined-multiline-flexible"
           label="Tu opninion nos interesa"
+          name="description"
           multiline
           maxRows={4}
+          onChange={(e) => onImputChange(e)}
         />
-        <Button variant="contained" sx={{ width: 30 }}>
+        <Button
+          variant="contained"
+          sx={{ width: 30 }}
+          onClick={() => console.log(rating)}
+        >
           Enviar
         </Button>
-        <ModalPaymentsComp />
+        {/*         <ModalPaymentsComp /> */}
       </Stack>
     </Container>
   );
