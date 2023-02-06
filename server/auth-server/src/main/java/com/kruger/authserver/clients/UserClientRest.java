c93d202f1fd47a42eb9cc93470e4abe290e70eec
Javier Vallejo <rjavier001@gmail.com>
2023-02-06 14:03:45 -0500

    user model updated controller updated

---
+++ server/auth-server/src/main/java/com/kruger/authserver/clients/UserClientRest.java
@@ -0,0 +1,24 @@
+package com.user.msvusers.clients;
+
+import com.user.msvusers.model.Order;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "msv-order")
+public interface OrderClientRest {
+
+    @GetMapping(value = "/{id}")
+    Order getOrder(@PathVariable("id") long id);
+
+    @PostMapping(value= "/user/{id}")
+    Order createOrder(@RequestBody Order order, @PathVariable("id") long customerId);
+
+    @PostMapping("/")
+    Order createO(@RequestBody Order order);
+
+    @GetMapping("/orders-by-user")
+    List<Order> getAllOrdersByUser(@RequestParam Iterable<Long> ids);
+
+}
