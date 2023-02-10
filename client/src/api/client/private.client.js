0487cc952c0186e7d71d3295490c961467aacb68
KevinVeliz <mario-veliz258934@hotmail.com>
2023-02-10 17:23:18 -0500

    Api change

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:8080/api/";
+// const baseURL = "http://localhost:8080/api/";
+const baseURL = "https://cc60-2800-bf0-2a7-118a-3882-c038-54a4-96a.sa.ngrok.io/api/"
 
 const privateClient = axios.create({
   baseURL,
