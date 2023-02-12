40500d3870c9ec4c1626a42cab8d61722c749b21
Javier Yanez <eddy.j.av@gmail.com>
2023-02-12 18:18:02 -0500

    :car: Fix Page dependencies

--- client/src/screens/ContactPage.jsx
+++ client/src/screens/ContactPage.jsx
@@ -6,14 +6,122 @@ import {
   CardContent,
   Typography,
 } from "@mui/material";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uyhf86h",
+        "template_g7mejle",
+        form.current,
+        "HrjJi1zhHn2omSP9m"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("mensaje enviado");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <iframe
-      width="100%"
-      height="100%"
-      src="https://us21.list-manage.com/contact-form?u=e572a91ffde66622c25cf1286&form_id=1a90789157311ee68d14e01f7fe05e61"
-    ></iframe>
+    <Grid textAlign="center">
+      <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Contact Us
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            gutterBottom
+          >
+            Nuestro equipo se pondrá en contacto en las próximas 24 horas.
+          </Typography>
+          <form ref={form} onSubmit={sendEmail}>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  placeholder="Enter first name"
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  type="text"
+                  name="user_name"
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  placeholder="Enter last name"
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  type="text"
+                  name="user_last"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  placeholder="Enter email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  type="email"
+                  name="user_email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  type="number"
+                  placeholder="Enter phone number"
+                  label="Phone"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  name="user_phone"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Message"
+                  multiline
+                  rows={4}
+                  placeholder="Type your message here"
+                  variant="outlined"
+                  fullWidth
+                  required
+                  name="message"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  value="Send"
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 };
 
