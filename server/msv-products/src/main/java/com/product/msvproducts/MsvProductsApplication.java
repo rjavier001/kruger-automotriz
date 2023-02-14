deed994fa27ac604717cf2b25381be3190f6e2df
Javier Yanez <eddy.j.av@gmail.com>
2023-02-13 19:25:03 -0500

    :car: Not.Function --add Serhc

--- server/msv-products/src/main/java/com/product/msvproducts/MsvProductsApplication.java
+++ server/msv-products/src/main/java/com/product/msvproducts/MsvProductsApplication.java
@@ -1,8 +1,8 @@
 package com.product.msvproducts;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MsvProductsApplication {
 
@@ -10,6 +10,15 @@ public class MsvProductsApplication {
         SpringApplication.run(MsvProductsApplication.class, args);
     }
 
+   /* public static void run() throws ExecutionException, InterruptedException, IOException {
+        try(SearchClient searchClient = DefaultSearchClient.create("STF3VI4F0V","ebafaf8b16373dded67356b9e639bc2a")){
+            {
+                SearchIndex<Product> index = searchClient.initIndex("krugermotors", Product.class);
+
+            }
+        }
+    }
+    */
 
 }
 
