ef7c38d611d532e5aeb06486a25541a0cf839ce7
KevinVeliz <mario-veliz258934@hotmail.com>
2023-02-15 21:52:55 -0500

    Add edit and delete product view

--- client/src/components/CardComp.jsx
+++ client/src/components/CardComp.jsx
@@ -253,7 +253,7 @@ const CardComp = ({ props }) => {
         </CardContent>
         <CardActions sx={uiConfigs.button}>
           <Stack direction={"row"} spacing={12}>
-            {location.pathname === "/" ? (
+            {location.pathname === "/viewAll" ? (
               <>
                 <NavLink
                   to={{ pathname: `/admin/products/edit/${id}` }}
